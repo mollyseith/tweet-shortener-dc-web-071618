2f2eeecea41508f9d8c1828e323905c4a939f09c
Molly Seither <molly.seither@centre.edu>
2018-07-16 06:24:03 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,52 @@
-# Write your code here.
+def dictionary
+  {
+"hello" => 'hi',
+"to" => '2',
+"two" => '2',
+"too" => '2',
+"for" => '4',
+"four" => '4',
+'be' => 'b',
+'you' => 'u',
+"at" => "@",
+"and" => "&"
+}
+end
+
+def word_substituter(tweet)
+    # replaces long words with their expected short form
+      tweet.split(" ").map do |word|
+        if dictionary.keys.include?(word.downcase)
+          word = dictionary[word.downcase]
+        else
+          word
+        end
+    end.join(" ")
+end
+
+def bulk_tweet_shortener(tweet)
+  tweet.each do |phrase|
+    phrase = word_substituter(phrase)
+    puts "#{phrase}"
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  tweet.split(" ").map do |phrase|
+    if phrase.length > 140
+      word_substituter(phrase)
+    elsif phrase.length < 130
+      phrase
+    end
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  tweet.split(" ").map do |phrase|
+    if phrase.length > 140
+      word_substituter(phrase)[0..140] + "..."
+    else
+      phrase
+    end
+  end.join(" ")
+end
